23299d3b1199338172828167fada68155a998191
Julio Hortencio <julio_hortencio@hotmail.com>
2024-04-25 23:13:16 -0300

    feature: criando model caracter

---
+++ lib/app/data/models/character_model.dart
@@ -0,0 +1,20 @@
+class Character {
+  int id;
+  String name;
+  String status;
+  String species;
+  String type;
+  String gender;
+  String image;
+  String created;
+
+  Character(
+      {required this.id,
+      required this.name,
+      required this.status,
+      required this.species,
+      required this.type,
+      required this.gender,
+      required this.image,
+      required this.created});
+}
