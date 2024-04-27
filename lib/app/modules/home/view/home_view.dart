20bacd4c53cd2e5313f8cef79ec827d8d681d2de
Julio Hortencio <julio_hortencio@hotmail.com>
2024-04-26 22:55:52 -0300

    refactor: criando a listagem dos personagens

--- lib/app/modules/home/view/home_view.dart
+++ lib/app/modules/home/view/home_view.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_rick_and_morty/app/modules/home/controller/character_controller.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class HomeView extends StatefulWidget {
+ const  HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final controller = CharacterController();
+
+  @override
+  void initState(){
+    super.initState();
+    controller.getAllCharacter();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +23,19 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.greenAccent,
         title: const Center(child:  Text('Rick and Morty')),
+      ),
+      body: (
+        ListView.builder(
+          itemCount: controller.character.length,
+          itemBuilder: (context, i){
+            final character = controller.character[i];
+            return Column(
+              children: [
+                Text(character.name, style: TextStyle(color: Colors.black),),
+               Image.network(character.image)
+              ],
+            );
+          })
       ),
     );
   }
